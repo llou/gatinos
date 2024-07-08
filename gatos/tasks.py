05e46c2fb0a7e81f4825e632658e3e7959bbd4ff
Jorge Monforte Gonzalez <yo@llou.net>
2024-07-08 22:33:43 +0200

    stub gatos automatic state change

--- gatos/tasks.py
+++ gatos/tasks.py
@@ -1,6 +1,8 @@
+from datetime import date
 from django.conf import settings
 from celery import shared_task
-from .models import Foto, Colonia
+from .models import Foto, Colonia, Gato
+from .flows import GatoFlow
 
 
 @shared_task()
@@ -26,3 +28,28 @@ def update_exif(colonia_slug):
     for foto in colonia.fotos.all():
         if not foto.exif:
             foto.update_exif()
+
+
+def get_ultima_fecha(gato):
+    fechas = gato.get_actividad()
+    if not fechas:
+        return
+    fechas.sort()
+    return fechas[-1]
+
+
+def update_estado_gato(gato):
+    tdelta = date.today() - get_ultima_fecha(gato)
+    flow = GatoFlow(gato)
+    if gato.estado == 'LIBRE' and tdelta > settings.PERIODO_DESAPARECIDO:
+        flow.desaparecer()
+    if gato.estado == 'DESAPARECIDO' and tdelta > settings.PERIODO_OLVIDADO:
+        flow.olvidar()
+
+
+def check_gatos_estado():
+    gatos = Gato.objects.filter(estado__in=["LIBRE",
+                                            "OLVIDADO",
+                                            "DESAPARECIDO"])
+    for gato in gatos:
+        update_estado_gato(gato)
